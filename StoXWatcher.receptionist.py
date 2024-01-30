f6a7004144741bd06129177c0776c88172d29bab
Sxw88 <sohxuanwei88@gmail.com>
2024-01-30 21:55:37 +0800

    Added basic rbac

--- StoXWatcher.receptionist.py
+++ StoXWatcher.receptionist.py
@@ -5,9 +5,7 @@ import logging
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-
-# Define a few command handlers. These usually take the two arguments update and
-# context.
+# TODO: help 
 
 def check_authorized(chatid):
 
@@ -27,7 +25,7 @@ def check_authorized(chatid):
 
 
 def get_authorized_list():
-    # Read file which contains a list of authorized users
+    # Reads file which contains a list of authorized users
     authorized_list = []
     
     with open('authorized.lst') as readfile:
@@ -36,11 +34,28 @@ def get_authorized_list():
     return authorized_list
 
 
+def get_admin_list():
+    # Reads file which contains a list of authorized users with admin privileges
+    adm_list = []
+    
+    with open('admin.lst') as readfile:
+        adm_list = [int(line.rstrip()) for line in readfile]
+
+    return adm_list
+
+
 # Message Handler which filters out unauthorized users
-async def restrict (update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def restrict_user (update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text="User Unauthorized."
+        )
+
+# Message Handler which filters out non-admin users
+async def restrict_admin (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Admin privilege is required for this action."
         )
 
 
@@ -70,15 +85,94 @@ async def check_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def update_authorized(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
-    """Updates the list of authorized users"""
-    current_list = chatid_filter.chat_ids
-    chatid_filter.remove_chat_ids(current_list)
+    try:
+        """Updates the list of authorized users"""
+        current_list = chatid_filter.chat_ids
+        chatid_filter.remove_chat_ids(current_list)
 
-    chatid_filter.add_chat_ids(get_authorized_list())
-    await update.message.reply_text("Updated List of Authorized IDs.")
+        chatid_filter.add_chat_ids(get_authorized_list())
+
+        """Updates the list of authorized admins"""
+        current_admin_list = admin_chatid_filter.chat_ids
+        admin_chatid_filter.remove_chat_ids(current_admin_list)
+
+        admin_chatid_filter.add_chat_ids(get_admin_list())
+
+        await update.message.reply_text("Updated List of Authorized IDs.")
+    
+    except Exception as e:
+        await update.message.reply_text("An error occured: " + str(e))
 
 
-# TODO: Command line to add authorized user and remove authorized user
+async def add_authorized(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    """Adds a user's Chat ID to authorized.lst"""
+    return_msg = "Adding User to Authorized Users List"
+    original_msg = return_msg
+
+    # Check if the argument supplied is an integer
+    try:
+        int_chatid = int(context.args[0])
+    except:
+        return_msg = 'Usage: /add_authorized <chat_id>'
+
+    # Checks if user is already in the list
+    with open('authorized.lst') as readfile:
+        for line in readfile:
+            if line.strip() == str(context.args[0]):
+                return_msg = "User is already authorized"
+
+    # Proceeds to add user to authorized list if:
+    #   1. Argument provided is valid (int)
+    #   2. User does not exist in the list yet
+    if return_msg == original_msg:
+        with open('authorized.lst', 'a') as writefile:
+            writefile.write(str(context.args[0]) + "\n")
+
+        chatid_filter.add_chat_ids(int(context.args[0]))
+
+    await update.message.reply_text(return_msg)
+
+
+async def remove_authorized(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    """Removes a user's Chat ID from authorized.lst"""
+    return_msg = "Removing User from Authorized Users List"
+    original_msg = return_msg
+
+    # Check if the argument supplied is an integer
+    try:
+        int_chatid = int(context.args[0])
+    except:
+        return_msg = 'Usage: /remove_authorized <chat_id>'
+
+    # Make sure that the user exists in the list in the first place
+    # also, use this opportunity to extract list of users for later use 
+    return_msg = "Error: User not found"
+    user_list = []
+
+    with open('authorized.lst') as readfile:
+        for line in readfile:
+            user_list.append(line.strip())
+
+            if line.strip() == str(context.args[0]):
+                return_msg = original_msg
+
+    # Proceeds to remove user from authorized list if:
+    #   1. Argument provided is valid (int)
+    #   2. User already exists in the list
+    if return_msg == original_msg:
+        user_list.remove(str(context.args[0]))
+
+        # Add the list back to the file
+        with open('authorized.lst', 'w') as writefile:
+            for user_id in user_list:
+                writefile.write(str(user_id) + "\n")
+        
+        # Remove user from existing filter (white)list
+        chatid_filter.remove_chat_ids(int(context.args[0]))
+
+    await update.message.reply_text(return_msg)
 
 
 #async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -96,15 +190,22 @@ def main() -> None:
     # Users are able to check their ID regardless authorized or not
     application.add_handler(CommandHandler("check_id", check_id))
 
-    # Applies restrictions to Chat IDs that are not found in authorized.lst
-    restrict_handler = MessageHandler(~chatid_filter, restrict)
-    application.add_handler(restrict_handler)
+    """Applies restrictions to Chat IDs that are not found in authorized.lst"""
+    restrict_user_handler = MessageHandler(~chatid_filter, restrict_user)
+    application.add_handler(restrict_user_handler)
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+    
+    """Another restriction handler to restrict Chat IDs which are not admins"""
+    restrict_admin_handler = MessageHandler(~admin_chatid_filter, restrict_admin)
+    application.add_handler(restrict_admin_handler)
+
     application.add_handler(CommandHandler("update_authorized", update_authorized))
- 
+    application.add_handler(CommandHandler("add_authorized", add_authorized))
+    application.add_handler(CommandHandler("remove_authorized", remove_authorized))
+
     # on non command i.e message - echo the message on Telegram
     #application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
@@ -124,8 +225,9 @@ if __name__ == "__main__":
     logging.getLogger("httpx").setLevel(logging.WARNING)
     logger = logging.getLogger(__name__)
 
-    # Initialize the chat ID filter object
+    # Initialize the chat ID filter objects
     chatid_filter = filters.Chat(get_authorized_list())
+    admin_chatid_filter = filters.Chat(get_admin_list())
 
     # Get the token for Telegram Bot
     with open('conf/telg_token', 'r') as token_file:
